9dcdb54009291970fbf8535909c21722dd7e5437
815822 <815822@SAIT225788IT.ACDM.DS.SAIT.CA>
2021-01-14 15:33:25 -0700

    New project creation Created new java application for Week 2 lab

---
+++ src/week02_lab/Week02_Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02_lab;
+
+/**
+ *
+ * @author 815822
+ */
+public class Week02_Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Week 2 Lab Submission");
+    }
+    
+}
